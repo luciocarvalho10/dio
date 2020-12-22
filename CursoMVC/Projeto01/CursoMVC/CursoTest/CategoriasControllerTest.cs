1681f1a9cba50ae678f9b0f9c3bc3e0fff6c8b3d
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2020-12-22 14:35:31 -0400

    downgrade para framework 3.1 e add test

---
+++ CursoMVC/Projeto01/CursoMVC/CursoTest/CategoriasControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using CursoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoTest
+{
+    class CategoriasControllerTest
+    {
+        private readonly Mock<DbSet<Categoria>> _mockSet;
+        private readonly Mock<Context> _mockContext;
+        private readonly Categoria _categoria;
+
+        public CategoriasControllerTest()
+        {
+            _mockSet = new Mock<DbSet<Categoria>>();
+            _mockContext = new Mock<Context>();
+            _categoria = new Categoria { Id = 1, Descricao = "Teste Categoria" };
+        }
+    }
+}
